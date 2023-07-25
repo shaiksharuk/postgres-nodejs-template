3d785773dbab6e45f5e42521d7fb934fa3540eb0
shaiksharuk <shaiksharuk998@gmail.com>
2023-07-25 14:15:11 +0530

    add edit message function

--- src/components/card-list.tsx
+++ src/components/card-list.tsx
@@ -1,3 +1,9 @@
+import { ChangeEventHandler, useState } from "react";
+
+const validate = (text: string): boolean => {
+    return !!text.match(/^[0-9 a-z]*$/)
+}
+
 type CardListProps = {
     items: Array<Item>
 
@@ -8,13 +14,47 @@ export type Item = {
 }
 
 export function Card({ item }: { item: Item }) {
-    return (
+
+    const [userText, setUserText] = useState(item.message);
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    const onCancelHandler = () => {
+        setUserText(item.message)
+;        toggleOnEdit();
+
+    }
+    const toggleOnEdit = () => {
+        setIsEditing(!isEditing);
+    }
+
+    const inputChangeHandler: ChangeEventHandler<HTMLInputElement> = (event) => {
+        const newInput = event.target.value;
+        if (!validate(newInput)) {
+            window.alert("Sepcial characters are not valid")
+            return;
+        }
+        setUserText(newInput);
+    }
+
+    const viewBlock = (
         <div>
             id: {item.id}
             <br/>
             message: {item.message}
+            <button onClick={toggleOnEdit}>edit</button>
         </div>
     )
+    const editBlock = (
+        <div>
+            <input type="text" value={userText} onChange={inputChangeHandler} />
+            <button>update</button>
+            <button onClick={onCancelHandler}  >cancel</button>
+
+        </div>
+    )
+    
+    return isEditing ? editBlock : viewBlock;
 }
 
 export default function CardList(props: CardListProps) {
